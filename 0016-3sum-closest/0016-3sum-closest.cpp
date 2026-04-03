3db3dc77ebf6f0990464d92007a6bb1a9b03f404
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-03 11:57:50 +0530

    Time: 15 ms (32.27%), Space: 14.1 MB (33.1%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,48 +1,24 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int ans=1e9;
-        for(int i=0;i<n;i++)
-        {
-            if(i==0 || nums[i]!=nums[i-1])
-            {
-                int k=solve(i,nums,target);
-                if(abs(target-k)<(abs(target-ans)))
-                {
-                    ans=k;
-                }
-            }
-        }
-        return ans;
-    }
+        sort(nums.begin(), nums.end());
+        int result = nums[0] + nums[1] + nums[2];
 
-    int solve(int i,vector<int>& nums, int target)
-    {
-        int ans=1e9;
-        for(int j=i+1;j<nums.size();j++)
-        {
-            int l=j,r=nums.size()-1;
-            while(l<r)
-            {
-                int closest_target=nums[i]+nums[l]+nums[r];
-                if(abs(target-closest_target)<(abs(target-ans)))
-                {
-                    ans=closest_target;
-                }
-                if(closest_target==target)
-                {
-                    return target;
-                }else if(closest_target>target)
-                {
-                    r--;
-                }else
-                {
-                    l++;
-                }
+        for (int i = 0; i < nums.size() - 2; i++) {
+            int left = i + 1, right = nums.size() - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (abs(target - sum) < abs(target - result))
+                    result = sum;
+
+                if (sum == target) return target;
+                else if (sum < target) left++;
+                else right--;
             }
         }
-        return ans;
+
+        return result;
     }
 };
