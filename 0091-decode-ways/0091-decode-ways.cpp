b896be824f3676acfd8ed7a64c90798e3769a883
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-06 18:01:48 +0530

    Time: 0 ms (100%), Space: 8.4 MB (91.74%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        int n=s.size();
-        vector<int>dp(n+1,INT_MAX);
-        function<int(int)>dfs=[&](int idx)
+    int dp[101];
+    int solve(string &s,int i,int n)
+    {
+        if(i>=n)return 1;
+        
+        if(s[i]=='0')return dp[i]=0;
+        if(dp[i]!=-1)return dp[i];
+        int ans=0;
+        ans+=solve(s,i+1,n);
+        if(i<n-1)
         {
-            if(idx>=n)
-                return 1;
-            if(s[idx]-'0'==0)return 0;
-            if(dp[idx]!=INT_MAX)
-                return dp[idx];
-            int ans=0;
-            if(s[idx]=='1')
-            {
-                ans+=dfs(idx+1);
-                if(idx+1<n)
-                    ans+=dfs(idx+2);
-            }else if(s[idx]=='2')
-            {
-                ans+=dfs(idx+1);
-                if(idx+1<n and s[idx+1]-'0'<=6)
-                    ans+=dfs(idx+2);
-            }
-            else
-            {
-                ans+=dfs(idx+1);
-            }
-            return dp[idx]=ans;
-        };
-        return dfs(0);
+             int k=(s[i]-'0')*10+s[i+1]-'0';
+             if(k<=26)
+             {
+                 ans+=solve(s,i+2,n);
+             }
+        }
+        return dp[i]=ans;
+    }
+    int numDecodings(string s) {
+       int n=s.length();memset(dp,-1,sizeof(dp)); return solve(s,0,n);
     }
 };
