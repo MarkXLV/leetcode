e310f3a8033879d09da7b68d3adbcaeb03921b80
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-06 19:01:49 +0530

    Time: 0 ms (100%), Space: 9.2 MB (71.87%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-int n,m,p;
-bool canmake(string &s1,string &s2,string &s3,int i,int j,int k)
-{
-    if(i==n and j==m and k==p)return true;
-    if(i<n and s1[i]==s3[k] and canmake(s1,s2,s3,i+1,j,k+1))return true;
-    if(j<m and s2[j]==s3[k] and canmake(s1,s2,s3,i,j+1,k+1))return true;return false;
-    
-}
     bool isInterleave(string s1, string s2, string s3) {
-        n=s1.length();m=s2.length();p=s3.length();
-        if(n+m!=p)return false;
-        // return canmake(s1,s2,s3,0,0,0);
-        int dp[n+1][m+1];
-        dp[0][0]=true;
-        for(int i=1;i<=n;i++)
-        dp[i][0]=dp[i-1][0] && s1[i-1]==s3[i-1];
-        for(int i=1;i<=m;i++)
-        dp[0][i]=dp[0][i-1] && s2[i-1]==s3[i-1];
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                dp[i][j]=dp[i-1][j] && s1[i-1]==s3[i+j-1] || dp[i][j-1] &&  s2[j-1]==s3[i+j-1];
+        int n1 = s1.size(), n2 = s2.size();
+        if (n1 + n2 != s3.size()) return false;
+        vector<vector<bool>> dp(n1 + 1, vector<bool>(n2 + 1, false));
+        
+        dp[0][0] = true;
+        
+        for (int i = 0; i <= n1; ++i) {
+            for (int j = 0; j <= n2; ++j) {
+                if (i > 0 && s1[i - 1] == s3[i + j - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j];
+                }
+                if (j > 0 && s2[j - 1] == s3[i + j - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i][j - 1];
+                }
             }
         }
-        return dp[n][m];
+        return dp[n1][n2];
     }
 };
