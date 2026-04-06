45603d97536e5cbea27893fd4540460458eab4a7
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-06 18:18:39 +0530

    Time: 0 ms (100%), Space: 9.3 MB (23.05%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -5,19 +5,18 @@ public:
         int n = s.size();
         vector<int> dp(n + 1, 0);
         dp[0] = 1; // Base case for empty string
-        
-        for (int i = 1; i < n; ++i) {
-            int oneDigit = stoi(s.substr(i, 1));
-            int twoDigits = stoi(s.substr(i - 1, 2));
+        dp[1]=  1;
+        for (int i = 2; i <= n; ++i) {
+            int oneDigit = stoi(s.substr(i-1,1));
+            int twoDigits = stoi(s.substr(i-2,2));
             
             if (oneDigit >= 1) {
                 dp[i] += dp[i - 1];
             }
             if (twoDigits >= 10 && twoDigits <= 26) {
-                // consider two integers "12"
-                    dp[i] += i>=2?dp[i - 2]:1;   
+                dp[i] += dp[i - 2];   
             }
         }
-        return dp[n-1];
+        return dp[n];
     }
 };
