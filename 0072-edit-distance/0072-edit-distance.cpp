dc962fb00224e8d889a91a56caf59785c60ebed6
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-06 15:32:58 +0530

    Time: 8 ms (47.4%), Space: 10.8 MB (88.35%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();int m=word2.size();
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++)
+        dp[i][0]=i;
+        for(int j=0;j<=m;j++)
+        dp[0][j]=j;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                int val=INT_MAX;
+                if(word1[i-1]==word2[j-1])
+                {
+                    val=dp[i-1][j-1];
+                }else
+                {
+                    val=min(val,1+dp[i-1][j]);
+                    val=min(val,1+dp[i][j-1]);
+                    val=min(val,1+dp[i-1][j-1]);
+                }
+                // int del=1+dp[i-1][j];
+                // int ins=1+dp[i][j-1];
+                // int rep=((word1[i-1]!=word2[j-1])?1:0)+dp[i-1][j-1];
+                // dp[i][j]=min(rep,min(del,ins));
+                // cout<<dp[i][j]<<' ';
+                dp[i][j]=val;
+            }
+        }
+        return dp[n][m];
+    }
+};
