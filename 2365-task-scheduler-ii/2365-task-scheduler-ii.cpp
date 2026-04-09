824f919a4eb58212b5d9ce224e6b20a222baccff
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-09 09:15:21 +0530

    Time: 54 ms (82%), Space: 106.1 MB (47.75%) - LeetHub

--- 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     long long taskSchedulerII(vector<int>& tasks, int space) {
-        long long day=0;
-        map<long long,long long>allowed;
-        int n=tasks.size();
-        for(int i=0;i<n;i++)
-        {
-            day=max(day,allowed[tasks[i]]);
+        unordered_map<int, long long> last_time;
+        long long day = 0;
+
+        for (int task : tasks) {
+            if (last_time.count(task)) {
+                day = max(day, last_time[task] + space + 1);
+            }
+            last_time[task] = day;
             day++;
-            allowed[tasks[i]]=day+1ll*space;
-            
         }
+
         return day;
     }
 };
