397006bdb9edc94b99f9d205b3c525045d87d3dd
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-09 18:50:41 +0530

    Time: 3 ms (73.07%), Space: 13 MB (63.41%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,25 +1,52 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n=s.size();
-        vector<int>dp(n+1,false);
-        dp[0]=true;
-        unordered_map<string,bool>mp;
-        for(auto x:wordDict)
-        mp[x]=true;
+    struct Trie
+    {
+        unordered_map<char,Trie*>child;
+        bool isend=false;
+    };
+    
+    Trie* build(vector<string>& wordDict)
+    {
+        Trie* root=new Trie();
 
+        for(auto xx:wordDict)
+        {
+            Trie* curr=root;
+            for(auto x:xx)
+            {
+                if(!curr->child.count(x))
+                    curr->child[x]=new Trie();
+                
+                curr=curr->child[x];
+            }
+            curr->isend=true;
+        }
+        return root;
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // construct trie for O(n) searching -- previous string generation was taking extra O(k) time.
+        Trie* root=build(wordDict);
+        int n=s.size();
+        vector<bool>dp(n+1,false);
+        dp[0]=true;
         for(int i=0;i<n;i++)
         {
-            string temp="";
-            for(int j=i;j>=0;j--)
+            if(!dp[i])continue;
+            Trie* curr=root;
+            for(int j=i;j<n;j++)
             {
-                temp=s[j]+temp;
-                if(mp[temp])
-                {
-                    dp[i+1]|=dp[j];
-                }
+                if(!curr->child.count(s[j]))
+                    break;
+
+                curr=curr->child[s[j]];
+
+                if(curr->isend)
+                  dp[j+1]=true;
             }
         }
         return dp[n];
+
     }
 };
