8a8bf666bc4dabb75cbe720916b0e403497bce29
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-14 14:51:00 +0530

    Time: 409 ms (10.09%), Space: 249.6 MB (18.16%) - LeetHub

--- 2421-number-of-good-paths/2421-number-of-good-paths.cpp
+++ 2421-number-of-good-paths/2421-number-of-good-paths.cpp
@@ -1,51 +1,48 @@
-class DSU
-{
+class DSU {
 public:
-    vector<int>parent;
-    vector<int>rank;
+    vector<int> parent, size;
 
     DSU(int n) {
         parent.resize(n);
-        rank.resize(n, 1);
-        for (int i = 0; i < n; i++) parent[i] = i;
+        size.resize(n, 1);
+        iota(parent.begin(), parent.end(), 0);
     }
 
-    
-    int find(int x)
-    {
-        return parent[x]==x?x:parent[x]=find(parent[x]);
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
     }
 
-    bool unionSet(int a,int b)
-    {
-        int pa=find(a);
-        int pb=find(b);
-        if(pa==pb)
-            return false;
-        if(rank[pa]>=rank[pb])
-        {
-            rank[pa]+=rank[pb];
-            parent[pb]=pa;
-        }else
-        {
-            rank[pb]+=rank[pa];
-            parent[pa]=pb;
-        }
+    void unionSet(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
 
-        return true;
+        if (pa == pb) return;
+
+        // union by size
+        if (size[pa] < size[pb]) swap(pa, pb);
+
+        parent[pb] = pa;
+        size[pa] += size[pb];
     }
 };
 class Solution {
 public:
     int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
+
         int n=vals.size();
+
         DSU dsu=DSU(n);
+
         map<int,vector<int>>mp;
         vector<vector<int>>adj(n);
+
         for(int i=0;i<n;i++)
         {
             mp[vals[i]].push_back(i);
         }
+        
         for(auto x:edges)
         {
             adj[x[0]].push_back(x[1]);
