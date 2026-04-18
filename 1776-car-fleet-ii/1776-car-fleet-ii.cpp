f988d5b5930a77a0020205eb59670ce64483a70d
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-18 21:10:30 +0530

    Time: 8 ms (85.11%), Space: 135.6 MB (20.39%) - LeetHub

--- 1776-car-fleet-ii/1776-car-fleet-ii.cpp
+++ 1776-car-fleet-ii/1776-car-fleet-ii.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     vector<double> getCollisionTimes(vector<vector<int>>& cars) {
-        int n=cars.size();
-        vector<double>ans(n,-1);
-        stack<int>st;
-        for(int i=n-1;i>=0;i--)
-        {
-            // while speed of current car is less than equal to cars on right it cannot intersect with them therefore we pop them
-            while(!st.empty() and cars[i][1]<=cars[st.top()][1])
-                st.pop();
-
-            while(!st.empty())
-            {
-                double time=(double)(cars[st.top()][0]-cars[i][0])/(cars[i][1]-cars[st.top()][1]);
-                if(ans[st.top()]==-1 || time<=ans[st.top()])
-                {
-                    ans[i]=time;
-                    break;
+        int n = cars.size();
+        vector<double> answer(n, -1);
+        stack<int> s;
+        for (int i = n - 1; i >= 0; --i) {
+            int position_i = cars[i][0];
+            int speed_i = cars[i][1];
+            while (!s.empty()) {
+                int j = s.top();
+                int position_j = cars[j][0];
+                int speed_j = cars[j][1];
+                if (speed_i <= speed_j) {
+                    s.pop();
+                } else {
+                    double collision_time = (double)(position_j - position_i) / (speed_i - speed_j);
+                    if (answer[j] == -1 || collision_time <= answer[j]) {
+                        answer[i]=collision_time;
+                        break;
+                    } else {
+                        s.pop();
+                    }
                 }
-                st.pop();
             }
-            st.push(i);
+            s.push(i);
         }
-        return ans;
+        return answer;
     }
 };
