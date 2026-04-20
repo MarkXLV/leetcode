1e531f237e8f23c49a6a58ced7275625aeb8e380
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-21 00:04:33 +0530

    Time: 10 ms (44.59%), Space: 14.4 MB (38.57%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -3,32 +3,49 @@ public:
     int swimInWater(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int ans=INT_MAX;
-        int dp[50][50][2501];
-        memset(dp,-1,sizeof(dp));
-        function<int(int,int,int)>dfs=[&](int x,int y,int mx)
-        {
-            if(x==n-1 and y==m-1)
-            {
-                return max(grid[x][y],mx);
-            }
-            if(dp[x][y][mx]!=-1)return dp[x][y][mx];
 
-            int res=INT_MAX;
+        auto canSwim=[&](int t)
+        {
+            vector<vector<bool>>vis(n,vector<bool>(m,false));
+            queue<pair<int,int>>q;
+            q.push({0,0});
+            vis[0][0]=true;
             vector<pair<int,int>>dir={{1,0},{-1,0},{0,-1},{0,1}};
-            int val=grid[x][y];
-            grid[x][y]=INT_MAX;
-            for(auto &[dx,dy]:dir)
+            while(!q.empty())
             {
-                int nx=x+dx;
-                int ny=y+dy;
-                if(nx<0 or ny<0 or nx>=n or ny>=m or grid[nx][ny]==INT_MAX)continue;
-                int cmax=dfs(nx,ny,max(mx,val));
-                res=min(res,cmax);
+                auto [x,y]=q.front();
+                q.pop();
+                if(x==n-1 and y==m-1)return true;
+                for(auto &[dx,dy]:dir)
+                {
+                    int nx=x+dx;
+                    int ny=y+dy;
+                    if(nx<0 or ny<0 or nx>=n or ny>=m or vis[nx][ny] or grid[nx][ny]>t)continue;
+                    q.push({nx,ny});
+                    vis[nx][ny]=true;
+                }
             }
-            grid[x][y]=val;
-            return dp[x][y][mx]=res;
+            return false;   
         };
-        return dfs(0,0,0);
+
+        int l=grid[0][0];
+        int r=n*n-1;
+        int ans=r;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(canSwim(mid))
+            {
+                ans=mid;
+                r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return ans;
     }
 };
+
+
