2065eb9c8ad78ed1d9db01e415b9c689339d6330
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-21 21:03:42 +0530

    Time: 14 ms (11.25%), Space: 20.6 MB (81.43%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        unordered_map<string,priority_queue<string,vector<string>,greater<string>>>adj;
-        for(auto x:tickets)
-        {
-            // cout<<x[0]<<' '<<x[1]<<endl;
-            adj[x[0]].push(x[1]);
-        }
-        vector<string>res;
-        function<void(string)>dfs=[&](string s)
-        {
-            while(!adj[s].empty())
-            {
-                auto t=adj[s].top();
-                adj[s].pop();
-                dfs(t);
-            }
-            res.push_back(s);
-        };
+        unordered_map<string, priority_queue<string, vector<string>, greater<string>>> adj;
+        vector<string> res;
         
+        // Build graph
+        for (auto& ticket : tickets) {
+            adj[ticket[0]].push(ticket[1]);
+        }
+
+        // DFS function
+        function<void(string)> dfs = [&](string airport) {
+            while (!adj[airport].empty()) {
+                string next = adj[airport].top();
+                adj[airport].pop();
+                dfs(next);
+            }
+            res.push_back(airport);
+        };
+
+        // Start DFS from 'JFK'
         dfs("JFK");
-        reverse(res.begin(),res.end());
+        reverse(res.begin(), res.end());
         return res;
     }
 };
