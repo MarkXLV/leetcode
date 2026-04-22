fd28b9ff5e098e1c28f84ad3acd2a76fb923a48d
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-23 00:51:17 +0530

    Time: 0 ms (100%), Space: 10.6 MB (76.98%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
-        int n=stones.size();
-        int total=0;
-        for(auto x:stones)total+=x;
-        int sum=total/2;
-        vector<vector<bool>>dp(n+1,vector<bool>(sum+1,false));
-        for(int i=0;i<=n;i++)
-        dp[i][0]=true;
-
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=sum;j++)
-            {
-                if(j-stones[i-1]>=0)
-                {
-                    dp[i][j]=dp[i-1][j-stones[i-1]] || dp[i-1][j];
-                }else
-                {
-                    dp[i][j]=dp[i-1][j];
-                }
+        int totalSum = accumulate(stones.begin(), stones.end(), 0);
+        int target = totalSum / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        
+        for (int stone : stones) {
+            for (int j = target; j >= stone; --j) {
+                dp[j] = dp[j] || dp[j - stone];
             }
         }
-
-        for(int i=sum;i>=0;i--)
-        {
-            if(dp[n][i])
-            {
-                return total-2*i;
+        
+        for (int j = target; j >= 0; --j) {
+            if (dp[j]) {
+                return totalSum - 2 * j;
             }
         }
         return 0;
-
     }
 };
