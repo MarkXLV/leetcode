596129105e3abdccb8995f46d9b749bb40921ca6
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-23 01:51:43 +0530

    Time: 0 ms (100%), Space: 10.2 MB (40.75%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -17,15 +17,21 @@ public:
 
         return dp[start][end]=sum-min(choice1,choice2);
     }
-    bool predictTheWinner(vector<int>& nums) {
-        long long sum=0;
-        int n=nums.size();
-        for(auto x:nums)sum+=x;
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        long long res=findMax(0,n-1,nums,sum,dp);
-        if(res>=(sum+1)/2)
-            return true;
-        else
-            return false;
-    };
+    bool predictTheWinner(vector<int>& piles) {
+        int n=piles.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        int k=solve(0,piles.size()-1,piles,dp);
+        if(k>=0)return true;
+        return false;
+    }
+    int solve(int l,int r,vector<int>& piles,vector<vector<int>>&dp)
+    {
+        if(l==r)return piles[l];
+        if(l>r)return 0;
+        int &ans=dp[l][r];
+        if(ans!=-1)return ans;
+        int first=piles[l]-solve(l+1,r,piles,dp);
+        int last=piles[r]-solve(l,r-1,piles,dp);
+        return ans=max(first,last);
+    }
 };
