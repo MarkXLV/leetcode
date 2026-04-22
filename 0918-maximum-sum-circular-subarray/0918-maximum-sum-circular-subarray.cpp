a4ec0b59be4fd69083b9be4941ab973fa05514fb
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-23 02:29:24 +0530

    Time: 0 ms (100%), Space: 43.8 MB (7.83%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int ans=INT_MIN;
-        int ans1=INT_MAX;
-        int currp=0;
-        int currn=0;
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            currp+=nums[i];
-            currn+=nums[i];
-            ans=max(ans,currp);
-            ans1=min(ans1,currn);
-            if(currp<0)currp=0;
-            if(currn>0)currn=0;
-        }
-        int k=sum!=ans1?sum-ans1:INT_MIN;
-        return max(ans,k);
+        int max_kadane = kadane(nums);
+        int total_sum = 0;
+        for (int num : nums) total_sum += num;
+        for (int& num : nums) num = -num;
+        int min_kadane = kadane(nums);
+        int circular_max = total_sum + min_kadane;
+        if (circular_max == 0) return max_kadane;
+        return max(max_kadane, circular_max);
     }
     
+private:
+    int kadane(vector<int>& arr) {
+        int current_max = arr[0], global_max = arr[0];
+        for (int i = 1; i < arr.size(); ++i) {
+            current_max = max(arr[i], current_max + arr[i]);
+            global_max = max(global_max, current_max);
+        }
+        return global_max;
+    }
 };
