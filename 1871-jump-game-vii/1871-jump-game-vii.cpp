f5c0dd1be0df380099b4cb6f071024d1378a3f21
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-23 03:21:08 +0530

    Time: 12 ms (47.19%), Space: 23.7 MB (53.15%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     bool canReach(string s, int minJump, int maxJump) {
         int n = s.size();
-        vector<bool> visited(n, false);
         queue<int> q;
         q.push(0);
-        visited[0] = true;
         int farthest = 0;
 
         while (!q.empty()) {
             int i = q.front(); q.pop();
 
             for (int j = max(i + minJump, farthest + 1); j <= min(i + maxJump, n - 1); ++j) {
-                if (s[j] == '0' && !visited[j]) {
+                if (s[j] == '0') {
                     if (j == n - 1) return true;
                     q.push(j);
-                    visited[j] = true;
                 }
             }
             farthest = min(i + maxJump, n - 1);
