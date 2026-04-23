49a2ae03a239d0b7870680d98913d95c4687b804
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-23 11:13:35 +0530

    Time: 0 ms (100%), Space: 9.4 MB (90.81%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     string predictPartyVictory(string senate) {
-        queue<int> radiantQueue, direQueue;
-        int n = senate.size();
+        int cnt = 0, i = 0;
 
-        // Initialize queues with indices of 'R' and 'D'
-        for (int i = 0; i < n; ++i) {
-            if (senate[i] == 'R')
-                radiantQueue.push(i);
-            else
-                direQueue.push(i);
+        while (i < senate.size()) {
+            char c = senate[i];
+            if (c == 'R') {
+                if (cnt < 0) {
+                    senate.push_back('D');
+                }
+                cnt++;
+            } else {
+                if (cnt > 0) {
+                    senate.push_back('R');
+                }
+                cnt--;
+            }
+            i++;
         }
 
-        // Simulate the voting process
-        while (!radiantQueue.empty() && !direQueue.empty()) {
-            int radiantIndex = radiantQueue.front(); radiantQueue.pop();
-            int direIndex = direQueue.front(); direQueue.pop();
-
-            if (radiantIndex < direIndex)
-                radiantQueue.push(radiantIndex + n);
-            else
-                direQueue.push(direIndex + n);
-        }
-
-        return radiantQueue.empty() ? "Dire" : "Radiant";
+        return cnt > 0 ? "Radiant" : "Dire";
     }
 };
