0dd58c8ae5e9da68fb5a0b30934c5c4bda7365d5
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-24 13:22:56 +0530

    Time: 27 ms (96.09%), Space: 56.3 MB (78.64%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,75 +1,45 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    vector<vector<int>>adj;
-    void dfs(int node,int par)
-    { 
-        for(auto x:adj[node])
-        {
-            if(x!=par)
-            {
-                dfs(x,node);
-                int k=dp[x][0]+1;
-                if(k>dp[node][0])
-                {
-                    dp[node][1]=dp[node][0];
-                    dp[node][0]=k;
-                }else if(k>dp[node][1])
-                {
-                    dp[node][1]=k;
-                }
-            }
-        }
-    }
-
-    void dfs1(int node,int par,int mx)
-    {
-        if(mx>dp[node][0])
-        {
-            dp[node][1]=dp[node][0];
-            dp[node][0]=mx;
-        }else if(mx>dp[node][1])
-        {
-            dp[node][1]=mx;
-        }
-
-        for(auto x:adj[node])
-        {
-            if(x!=par)
-            {
-                // node in maxumum path
-                if(dp[x][0]+1==dp[node][0])
-                {
-                    dfs1(x,node,dp[node][1]+1);
-                }else
-                {
-                    dfs1(x,node,dp[node][0]+1);
-                }
-            }
-        }
-    }
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        dp.assign(n,vector<int>(2,0));
-        adj.assign(n,vector<int>());
-        for(auto &x:edges)
-        {
-            adj[x[0]].push_back(x[1]);
-            adj[x[1]].push_back(x[0]);
-        }
-        dfs(0,-1);
-        dfs1(0,-1,0);
-        int mn=INT_MAX;
-        vector<int>res;
-        for(int i=0;i<n;i++)
-        {
-            mn=min(mn,dp[i][0]);
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(dp[i][0]==mn)
-                res.push_back(i);
-        }
-        return res;
+        if (n == 1) return {0};
 
+        vector<vector<int>> adj(n);
+        for (auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+        vector<int> edge_cnt(n);
+        queue<int> leaves;
+
+        for (int i = 0; i < n; ++i) {
+            edge_cnt[i] = adj[i].size();
+            if (adj[i].size() == 1)
+                leaves.push(i);
+        }
+
+        while (!leaves.empty()) {
+            if (n <= 2) {
+                vector<int> result;
+                while (!leaves.empty()) {
+                    result.push_back(leaves.front());
+                    leaves.pop();
+                }
+                return result;
+            }
+            int size = leaves.size();
+            for (int i = 0; i < size; ++i) {
+                int node = leaves.front();
+                leaves.pop();
+                --n;
+                for (int& nei : adj[node]) {
+                    --edge_cnt[nei];
+                    if (edge_cnt[nei] == 1)
+                        leaves.push(nei);
+                }
+            }
+        }
+
+        return {};
     }
 };
