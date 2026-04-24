f8c5bd1cb52933b74f033c84e1bf11df250fb8ab
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-24 16:21:35 +0530

    Time: 71 ms (34.05%), Space: 22.2 MB (98.52%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,37 +1,53 @@
 class Solution {
+private:
+    int ROWS, COLS;
+    vector<vector<int>> heights;
+    vector<vector<bool>> visited;
+    vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int rows = heights.size(), cols = heights[0].size();
-        vector<vector<int>> dist(rows, vector<int>(cols, INT_MAX));
-        dist[0][0] = 0;
+        this->heights = heights;
+        this->ROWS = heights.size();
+        this->COLS = heights[0].size();
+        this->visited = vector<vector<bool>>(ROWS, vector<bool>(COLS, false));
 
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> minHeap;
-        minHeap.push({0, 0, 0}); // {diff, row, col}
-
-        vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-        while (!minHeap.empty()) {
-            auto curr = minHeap.top();
-            minHeap.pop();
-            int diff = curr[0], r = curr[1], c = curr[2];
-
-            if (r == rows - 1 && c == cols - 1) return diff;
-            if (dist[r][c] < diff) continue;
-
-            for (auto& dir : directions) {
-                int newR = r + dir[0], newC = c + dir[1];
-                if (newR < 0 || newC < 0 || newR >= rows || newC >= cols) {
-                    continue;
-                }
-
-                int newDiff = max(diff, abs(heights[r][c] - heights[newR][newC]));
-                if (newDiff < dist[newR][newC]) {
-                    dist[newR][newC] = newDiff;
-                    minHeap.push({newDiff, newR, newC});
-                }
+        int l = 0, r = 1'000'000, res = r;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            for (auto& row : visited) {
+                fill(row.begin(), row.end(), false);
+            }
+            if (dfs(0, 0, mid)) {
+                res = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
+        return res;
+    }
 
-        return 0;
+private:
+    bool dfs(int r, int c, int limit) {
+        if (r == ROWS - 1 && c == COLS - 1) {
+            return true;
+        }
+
+        visited[r][c] = true;
+        for (const auto& dir : directions) {
+            int newR = r + dir[0];
+            int newC = c + dir[1];
+            if (newR < 0 || newC < 0 || newR >= ROWS || newC >= COLS || visited[newR][newC]) {
+                continue;
+            }
+            if (abs(heights[newR][newC] - heights[r][c]) > limit) {
+                continue;
+            }
+            if (dfs(newR, newC, limit)) {
+                return true;
+            }
+        }
+        return false;
     }
 };
