6675cca71de92c7986f153be12ecc45649569645
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-27 18:13:36 +0530

    Time: 0 ms (100%), Space: 35.7 MB (91.93%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int n = arr.size();
-        int idx = 0;
-        for (int i = 1; i < n; i++) {
-            if (abs(x - arr[idx]) > abs(x - arr[i])) {
-                idx = i;
+        int l = 0, r = arr.size() - 1;
+        while (r - l >= k) {
+            if (abs(x - arr[l]) <= abs(x - arr[r])) {
+                r--;
+            } else {
+                l++;
             }
         }
-
-        vector<int> res = {arr[idx]};
-        int l = idx - 1, r = idx + 1;
-
-        while (res.size() < k) {
-            if (l >= 0 && r < n) {
-                if (abs(x - arr[l]) <= abs(x - arr[r])) {
-                    res.push_back(arr[l--]);
-                } else {
-                    res.push_back(arr[r++]);
-                }
-            } else if (l >= 0) {
-                res.push_back(arr[l--]);
-            } else if (r < n) {
-                res.push_back(arr[r++]);
-            }
-        }
-
-        sort(res.begin(), res.end());
-        return res;
+        return vector<int>(arr.begin() + l, arr.begin() + r + 1);
     }
 };
