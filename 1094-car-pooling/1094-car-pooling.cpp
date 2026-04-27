6a5f4e938be3c8da6368fc991748532c3646476b
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-27 17:20:38 +0530

    Time: 0 ms (100%), Space: 14.2 MB (80.03%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<pair<int, int>> points;
-        for (const auto& trip : trips) {
-            int passengers = trip[0], start = trip[1], end = trip[2];
-            points.emplace_back(start, passengers);
-            points.emplace_back(end, -passengers);
-        }
-
-        sort(points.begin(), points.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
-            return a.first == b.first ? a.second < b.second : a.first < b.first;
+        sort(trips.begin(), trips.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
         });
 
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap; // [end, numPassengers]
         int curPass = 0;
-        for (const auto& point : points) {
-            curPass += point.second;
+
+        for (const auto& trip : trips) {
+            int numPass = trip[0], start = trip[1], end = trip[2];
+
+            while (!minHeap.empty() && minHeap.top().first <= start) {
+                curPass -= minHeap.top().second;
+                minHeap.pop();
+            }
+
+            curPass += numPass;
             if (curPass > capacity) {
                 return false;
             }
+
+            minHeap.emplace(end, numPass);
         }
 
         return true;
