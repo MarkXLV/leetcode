8b60e18f5c524863096a339163a837ec82dfc204
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-27 17:18:20 +0530

    Time: 0 ms (100%), Space: 14.3 MB (80.03%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,60 +1,25 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        int n=trips.size();
-        vector<pair<int,int>>pick;
-        vector<pair<int,int>>drop;
-        unordered_map<int,int>pickm;
-        unordered_map<int,int>dropm;
-        for(auto x:trips)
-        {
-            // pickm[x[1]]+=x[0];
-            // dropm[x[2]]+=x[0];
-            pick.push_back({x[1],x[0]});
-            drop.push_back({x[2],x[0]});
+        vector<pair<int, int>> points;
+        for (const auto& trip : trips) {
+            int passengers = trip[0], start = trip[1], end = trip[2];
+            points.emplace_back(start, passengers);
+            points.emplace_back(end, -passengers);
         }
 
-        // for(auto &[u,v]:pickm)
-        // {
-        //     pick.push_back({u,v});
-        // }
+        sort(points.begin(), points.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+            return a.first == b.first ? a.second < b.second : a.first < b.first;
+        });
 
-        // for(auto &[u,v]:dropm)
-        // {
-        //     drop.push_back({u,v});
-        // }
-
-        sort(pick.begin(),pick.end());
-        sort(drop.begin(),drop.end());
-        int i=0;int j=0;
-        while(i<pick.size() and j<drop.size())
-        {
-            // cout<<pick[i].first<<' '<<drop[j].first<<endl;
-            if(pick[i].first<drop[j].first)
-            {
-                capacity-=pick[i].second;
-                i++;
-            }else if(pick[i].first>drop[j].first)
-            {
-                capacity+=drop[j].second;
-                j++;
-            }else
-            {
-                capacity+=drop[j].second;
-                // capacity-=pick[i].second;
-                // i++;
-                j++;
+        int curPass = 0;
+        for (const auto& point : points) {
+            curPass += point.second;
+            if (curPass > capacity) {
+                return false;
             }
-            // cout<<capacity<<endl;
-            if(capacity<0)
-            return false;
         }
+
         return true;
     }
 };
-
-// 1 2 , 5 3 
-// 5 2 , 7 3
-
-// 2 3,4 6,8 9
-// 6 4,8 3,9 10
