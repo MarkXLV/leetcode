a8c252f61523636e1b228bd3bcf3af5b326e37be
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-04-29 15:20:25 +0530

    Time: 3 ms (71.27%), Space: 18.9 MB (28.27%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adjList(numCourses);
-        vector<int> indegree(numCourses, 0);
-        for (auto& pre : prerequisites) {
-            adjList[pre[1]].push_back(pre[0]);
-            indegree[pre[0]]++;
+        unordered_map<int,list<int>> adjList;
+        vector<int>res;
+        int n=numCourses;
+        vector<int> indegree(n, 0);
+
+        for (int i = 0; i < prerequisites.size(); i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+
+            adjList[v].push_back(u);
+            indegree[u]++;
         }
 
         queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) q.push(i);
-        }
+        int cnt = 0;
 
-        vector<int> order;
-        while (!q.empty()) {
-            int course = q.front(); q.pop();
-            order.push_back(course);
-            for (auto& next : adjList[course]) {
-                if (--indegree[next] == 0) {
-                    q.push(next);
-                }
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
             }
         }
 
-        if (order.size() == numCourses) {
-            return order;
-        } else {
-            return {};
+        while (!q.empty()) {
+            int front = q.front();
+            res.push_back(front);
+            q.pop();
+            cnt++;
+
+            for (auto &neigh : adjList[front]) {
+                indegree[neigh]--;
+                if (indegree[neigh] == 0) {
+                    q.push(neigh);
+                }
+            }
         }
+        vector<int>empty;
+
+        return res.size()==n?res:empty;
     }
 };
