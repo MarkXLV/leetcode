5cc17b6cb2278f885840267c753a18805fa12039
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-05-21 11:55:39 +0530

    Time: 300 ms (34.67%), Space: 233.1 MB (7.16%) - LeetHub

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -0,0 +1,59 @@
+struct Node{
+    unordered_map<char,Node*>mp;
+    bool isend=false;
+};
+class Trie
+{
+    private:
+        Node* root;
+    public:
+        Trie(){
+            root=new Node();
+        };
+        void insert(vector<int>&a)
+        {
+            for(auto x:a)
+            {
+                string s=to_string(x);
+                Node* temp=root;
+                for(auto xx:s)
+                {
+                    if(!temp->mp[xx])
+                    {
+                        temp->mp[xx]=new Node();
+                    }
+                    temp=temp->mp[xx];
+                }
+                temp->isend=true;
+            }
+        }
+        int find(int x)
+        {
+            string s=to_string(x);
+            Node* temp=root;
+            int cn=0;
+            for(auto xx:s)
+            {
+                if(!temp->mp[xx])
+                    return cn;
+                temp=temp->mp[xx];
+                cn++;
+            }
+            return cn;
+        }
+};
+
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        Trie trie;
+        trie.insert(arr1);
+        int ans=0;
+        for(auto x:arr2)
+        {
+            int k=trie.find(x);
+            ans=max(ans,k);
+        }
+        return ans;
+    }
+};
