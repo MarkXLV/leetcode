e8731623fd3121bad074c46f4a8e13bf77689ead
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-06-16 10:53:33 +0530

    Time: 5 ms (45.83%), Space: 40.3 MB (17.05%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    string processStr(string str) {
-        string s="";
-        for(auto x:str)
-        {
-            if(x=='*')
-                s=s.substr(0,s.size()-1);
-            else if(x=='#')
-                s=s+s;
-            else if(x=='%')
-                reverse(s.begin(),s.end());
-            else
-                s+=x;
+    string processStr(string s) {
+        string result = "";
+        for (auto it : s) {
+            if (it == '*') {
+                if (result.size()) {
+                    result.pop_back();
+                }
+            } else if (it == '#') {
+                result += result;
+            } else if (it == '%') {
+                result = string(result.rbegin(), result.rend());
+            } else {
+                result += it;
+            }
         }
-        return s;
+        return result;
     }
 };
