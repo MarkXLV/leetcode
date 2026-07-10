40a4289696c19915a2073467abcca2d5582ba736
shashank <102664828+MarkXLV@users.noreply.github.com>
2026-07-10 14:09:30 +0530

    Time: 8 ms (80.39%), Space: 224.2 MB (66.3%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,56 +1,20 @@
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        int i=0;int j=0;
-        vector<int>g[n];
-        while(j<n and i<n)
-        {
-            if(i==j)
-            {
-                j++;
-            }else if(abs(nums[i]-nums[j])<=maxDiff)
-            {
-                g[i].push_back(j);
-                g[j].push_back(i);
-                j++;
-            }else
-            {
-                i++;
+        vector<int> component(n, 0);
+        int compNo = 0;
+        for(int i = 1; i < n; i++){
+            if(nums[i] - nums[i - 1] > maxDiff){
+                compNo++;
             }
+            component[i] = compNo;
         }
-        vector<int>col(n,-1);
-        int cn=1;
-        function<void(int,int)>dfs=[&](int node,int cn)
-        {
-            col[node]=cn;
-            for(auto x:g[node])
-            {
-                if(col[x]==-1)
-                    dfs(x,cn);
-            }
-        };
-        for(int i=0;i<n;i++)
-        {
-            if(col[i]==-1)
-            {
-                dfs(i,cn);
-                cn++;
-            }
+        vector<bool> sol;
+
+        for(auto &it : queries){
+            sol.push_back(component[it[0]] == component[it[1]]);
         }
 
-        vector<bool>res;
-        for(auto x:queries)
-        {
-            int i=x[0];
-            int j=x[1];
-            if(col[i]==col[j])
-            {
-                res.push_back(true);
-            }else
-            {
-                res.push_back(false);
-            }
-        }
-        return res;
+        return sol;
     }
 };
